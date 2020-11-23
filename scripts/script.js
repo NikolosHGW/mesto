672a0fffcc59aa2fac53a23c60f0c738f9fd7aa8
Ник Николос <nic23172@gmail.com>
2020-11-23 16:42:03 +0300

    fix: перенести все DOM элементы в переменные

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@ let eddButton = document.querySelector(".profile__edit-button");
 let clsButton = popup.querySelector(".popup__close-icon");
 let profile = document.querySelector(".profile");
 let formElement = document.querySelector(".popup__form");
+let nameInput = popup.querySelector(".popup__edit-name");
+let jobInput = popup.querySelector(".popup__edit-job");
+let profName = profile.querySelector(".profile__name");
+let profJob = profile.querySelector(".profile__job");
 
 function clsPopup() {
   popup.classList.remove("popup_opened");
@@ -10,8 +14,8 @@ function clsPopup() {
 
 eddButton.addEventListener("click", function() {
   popup.classList.add("popup_opened");
-  popup.querySelector(".popup__edit-name").value = profile.querySelector(".profile__name").textContent;
-  popup.querySelector(".popup__edit-job").value = profile.querySelector(".profile__job").textContent;
+  nameInput.value = profName.textContent;
+  jobInput.value = profJob.textContent;
 });
 
 clsButton.addEventListener("click", function() {
@@ -21,11 +25,8 @@ clsButton.addEventListener("click", function() {
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = popup.querySelector(".popup__edit-name");
-  let jobInput = popup.querySelector(".popup__edit-job");
-
-  profile.querySelector(".profile__name").textContent = nameInput.value;
-  profile.querySelector(".profile__job").textContent = jobInput.value;
+  profName.textContent = nameInput.value;
+  profJob.textContent = jobInput.value;
   clsPopup();
 }
 
