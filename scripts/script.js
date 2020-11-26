10d8efb7ddb1abbe66b21bdc0d9f5ed519190495
Ник Николос <nic23172@gmail.com>
2020-11-26 12:16:07 +0300

    fix: исправить слушатели

--- scripts/script.js
+++ scripts/script.js
@@ -8,26 +8,27 @@ let jobInput = popup.querySelector(".popup__edit-job");
 let profName = profile.querySelector(".profile__name");
 let profJob = profile.querySelector(".profile__job");
 
-function clsPopup() {
-  popup.classList.remove("popup_opened");
-}
-
-eddButton.addEventListener("click", function() {
-  popup.classList.add("popup_opened");
+//Функция для открытия Popup окна
+function opnPopup() {
+  popup.classList.add("popup_opened"); //добавляет стиль с display:flex, чтобы перекрыть display:none
   nameInput.value = profName.textContent;
   jobInput.value = profJob.textContent;
-});
+}
 
-clsButton.addEventListener("click", function() {
-  clsPopup();
-});
+//Функция для закрытия Popup окна
+function clsPopup() {
+  popup.classList.remove("popup_opened"); // убирает модификатор со стилем display:flex
+}
 
+//Функция для отправки данных из инпут полей Popup в profile
 function formSubmitHandler(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // сбрасывает стандартную отправку формы
 
   profName.textContent = nameInput.value;
   profJob.textContent = jobInput.value;
   clsPopup();
 }
 
+eddButton.addEventListener("click", opnPopup);
+clsButton.addEventListener("click", clsPopup);
 formElement.addEventListener('submit', formSubmitHandler);
