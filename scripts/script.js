498d6cc63f46d3808cf09ee1f9b4b37b30203063
Ник Николос <nic23172@gmail.com>
2020-12-03 19:01:42 +0300

    feat: add viewing images

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 const popupEdd = document.querySelector(".popup_edd");
 const popupAdd = document.querySelector(".popup_add");
+const popupImg = document.querySelector(".popup-img");
 const eddButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const clsButtonEdd = popupEdd.querySelector(".popup__close-icon");
 const clsButtonAdd = popupAdd.querySelector(".popup__close-icon");
+const clsButtonImg = popupImg.querySelector(".popup-img__close-icon");
 const profile = document.querySelector(".profile");
 const formElement = popupEdd.querySelector(".popup__form");
 const formElementAdd = popupAdd.querySelector(".popup__form");
@@ -13,6 +15,8 @@ const cardNameInput = popupAdd.querySelector(".popup__input_el_card-name");
 const imgLinkInput = popupAdd.querySelector(".popup__input_el_img-link");
 const profName = profile.querySelector(".profile__name");
 const profJob = profile.querySelector(".profile__job");
+const bigImg = popupImg.querySelector(".popup-img__img");
+const captionBigImg = popupImg.querySelector(".popup-img__caption");
 const elementTemplate = document.querySelector("#element-template").content;
 const elementsSection = document.querySelector(".elements");
 const initialCards = [
@@ -51,6 +55,7 @@ initialCards.forEach(item => {
     evt.target.classList.toggle("element__like-button_active");
   });
   elementItem.querySelector(".element__del-button").addEventListener("click", deleteCard);
+  elementItem.querySelector(".element__img-button").addEventListener("click", viewImg);
   elementsSection.append(elementItem);
 });
 
@@ -73,6 +78,7 @@ function opnAddPopup() {
 function clsPopup() {
   popupEdd.classList.remove("popup_opened"); // убирает модификатор со стилем display:flex
   popupAdd.classList.remove("popup_opened");
+  popupImg.classList.remove("popup-img_opened");
 }
 
 //Функция для отправки данных из инпут полей Popup в profile
@@ -87,6 +93,13 @@ function formSubmitHandler(evt) {
 //Функция для удаления карточек
 function deleteCard(evt) {
   evt.target.closest(".element").remove();
+}
+
+//Функция для открытия картинки на 75% размера экрана
+function viewImg(evt) {
+  bigImg.src = evt.target.src;
+  captionBigImg.textContent = evt.target.parentElement.parentElement.querySelector(".element__heading").textContent;
+  popupImg.classList.add("popup-img_opened");
 }
 
 //Функция создания карточек
@@ -109,5 +122,6 @@ eddButton.addEventListener("click", opnEddPopup);
 addButton.addEventListener("click", opnAddPopup);
 clsButtonEdd.addEventListener("click", clsPopup);
 clsButtonAdd.addEventListener("click", clsPopup);
+clsButtonImg.addEventListener("click", clsPopup);
 formElement.addEventListener("submit", formSubmitHandler);
 formElementAdd.addEventListener("submit", formAddSubmitHandler);
