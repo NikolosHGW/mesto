507956c66584e289186c6517458224e1aec9965f
Ник Николос <nic23172@gmail.com>
2021-01-05 11:02:19 +0300

    refactor: OOP for cards

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,7 +1,7 @@
 export default class Card {
   constructor(data, cardSelector) {
-    this._name = data.name;
-    this._link = data.link;
+    this.name = data.name;
+    this.link = data.link;
     this._cardSelector = cardSelector;
   }
 
@@ -16,7 +16,7 @@ export default class Card {
     this._headingElement = this._element.querySelector(".element__heading");
     this._buttonLikeElement = this._element.querySelector(".element__like-button");
     this._buttonDelElement = this._element.querySelector(".element__del-button");
-    this._buttonImgElement = this._element.querySelector(".element__img-button");
+    this.buttonImgElement = this._element.querySelector(".element__img-button");
     this._bigImg = document.querySelector(".popup-img__img");
     this._captionBigImg = document.querySelector(".popup-img__caption");
     this._popupImg = document.querySelector(".popup_img");
@@ -32,48 +32,48 @@ export default class Card {
     evt.target.closest(".element").remove();
   }
 
-  //Функция для открытия Popup окон
-  _openPopup(popup) {
-    popup.classList.add("popup_opened"); //добавляет стиль с visibility:visible, чтобы перекрыть visibility:hide
-    this._addAnyListeners(this._body, "keydown", this._closePopupWithEscape);
-  }
+  // //Функция для открытия Popup окон
+  // _openPopup(popup) {
+  //   popup.classList.add("popup_opened"); //добавляет стиль с visibility:visible, чтобы перекрыть visibility:hide
+  //   this._addAnyListeners(this._body, "keydown", this._closePopupWithEscape);
+  // }
 
-  //Функция для закрытия Popup окон
-  _closePopup(popup) {
-    popup.classList.remove("popup_opened"); // убирает модификатор со стилем visibility:visible
-    this._removeAnyListeners(this._body, "keydown", this._closePopupWithEscape);
-  }
+  // //Функция для закрытия Popup окон
+  // _closePopup(popup) {
+  //   popup.classList.remove("popup_opened"); // убирает модификатор со стилем visibility:visible
+  //   this._removeAnyListeners(this._body, "keydown", this._closePopupWithEscape);
+  // }
 
-  _closePopupWithEscape(evt) {
-    if (evt.key == "Escape") {
-      const openedPopup = document.querySelector('.popup_opened')
-      if (openedPopup) {
-        this._closePopup(openedPopup);
-      }
-    }
-  }
+  // _closePopupWithEscape(evt) {
+  //   if (evt.key == "Escape") {
+  //     const openedPopup = document.querySelector('.popup_opened')
+  //     if (openedPopup) {
+  //       this._closePopup(openedPopup);
+  //     }
+  //   }
+  // }
 
-  //Функция для добавления слушателя на любой элемент
-  _addAnyListeners(element, eventString, func) {
-    element.addEventListener(eventString, func);
-  }
+  // //Функция для добавления слушателя на любой элемент
+  // _addAnyListeners(element, eventString, func) {
+  //   element.addEventListener(eventString, func);
+  // }
 
-  //Функция для удаления слушателя с любого элемента
-  _removeAnyListeners(element, eventString, func) {
-    element.removeEventListener(eventString, func);
-  }
+  // //Функция для удаления слушателя с любого элемента
+  // _removeAnyListeners(element, eventString, func) {
+  //   element.removeEventListener(eventString, func);
+  // }
 
-  _openPopupImg() {
-    this._bigImg.src = this._link;
-    this._bigImg.alt = this._name;
-    this._captionBigImg.textContent = this._name;
-    this._openPopup(this._popupImg);
-  }
+  // _openPopupImg() {
+  //   this._bigImg.src = this._link;
+  //   this._bigImg.alt = this._name;
+  //   this._captionBigImg.textContent = this._name;
+  //   this._openPopup(this._popupImg);
+  // }
 
   _setEventListeners(){
     this._buttonLikeElement.addEventListener("click", evt => {this._toggleButtonActive(evt)});
     this._buttonDelElement.addEventListener("click", evt => {this._deleteCard(evt)});
-    this._buttonImgElement.addEventListener("click", () => {this._openPopupImg()});
+    // this._buttonImgElement.addEventListener("click", () => {this._openPopupImg()});
     // this._buttonClsImg.addEventListener("click", () => {this._closePopup(this._popupImg)});
   }
 
@@ -82,7 +82,7 @@ export default class Card {
     this._initialVariables();
     this._setEventListeners();
 
-    this._imgElement.src = this._link;
+    this._imgElement.src = this.link;
     this._imgElement.alt = "Загруженная картинка: " + this.name;
     this._headingElement.textContent = this.name;
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 import Card from "./Card.js";
-// const body = document.querySelector(".body");
+const body = document.querySelector(".body");
 const popups = Array.from(document.querySelectorAll(".popup"));
 const popupEdt = document.querySelector(".popup_edd");
 const popupAdd = document.querySelector(".popup_add");
@@ -18,8 +18,8 @@ const cardNameInput = popupAdd.querySelector(".popup__input_el_card-name");
 const imgLinkInput = popupAdd.querySelector(".popup__input_el_img-link");
 const profName = profile.querySelector(".profile__name");
 const profJob = profile.querySelector(".profile__job");
-// const bigImg = popupImg.querySelector(".popup-img__img"); // Delete
-// const captionBigImg = popupImg.querySelector(".popup-img__caption"); //Delete
+const bigImg = popupImg.querySelector(".popup-img__img"); // Delete
+const captionBigImg = popupImg.querySelector(".popup-img__caption"); //Delete
 // const elementTemplate = document.querySelector("#element-template").content; //Удалить
 const elementsContainer = document.querySelector(".elements");
 const forms = document.forms;
@@ -63,17 +63,17 @@ const initialCards = [
 ];
 
 
-// //Функция для открытия Popup окон
-// function openPopup(popup) {
-//   popup.classList.add("popup_opened"); //добавляет стиль с visibility:visible, чтобы перекрыть visibility:hide
-//   addAnyListeners(body, "keydown", closePopupWithEscape);
-// }
+//Функция для открытия Popup окон
+function openPopup(popup) {
+  popup.classList.add("popup_opened"); //добавляет стиль с visibility:visible, чтобы перекрыть visibility:hide
+  addAnyListeners(body, "keydown", closePopupWithEscape);
+}
 
-// //Функция для закрытия Popup окон
-// function closePopup(popup) {
-//   popup.classList.remove("popup_opened"); // убирает модификатор со стилем visibility:visible
-//   removeAnyListeners(body, "keydown", closePopupWithEscape);
-// }
+//Функция для закрытия Popup окон
+function closePopup(popup) {
+  popup.classList.remove("popup_opened"); // убирает модификатор со стилем visibility:visible
+  removeAnyListeners(body, "keydown", closePopupWithEscape);
+}
 
 // //Функция для отправки данных из инпут полей Popup в profile
 // function formSubmitHandler(evt) {
@@ -122,25 +122,32 @@ function addCard(container, cardElement, append = false) {
 //   closePopup(popupAdd);
 // }
 
-// //Функция для закрытия попапа при событии keydown равное esc DELETE
-// function closePopupWithEscape(evt) {
-//   if (evt.key == "Escape") {
-//     const openedPopup = document.querySelector('.popup_opened')
-//     if (openedPopup) {
-//       closePopup(openedPopup);
-//     }
-//   }
-// }
+//Функция для закрытия попапа при событии keydown равное esc DELETE
+function closePopupWithEscape(evt) {
+  if (evt.key == "Escape") {
+    const openedPopup = document.querySelector('.popup_opened')
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
 
-// //Функция для добавления слушателя на любой элемент DELETE
-// function addAnyListeners(element, eventString, func) {
-//   element.addEventListener(eventString, func);
-// }
+//Функция для добавления слушателя на любой элемент DELETE
+function addAnyListeners(element, eventString, func) {
+  element.addEventListener(eventString, func);
+}
 
-// //Функция для удаления слушателя с любого элемента DELETE
-// function removeAnyListeners(element, eventString, func) {
-//   element.removeEventListener(eventString, func);
-// }
+//Функция для удаления слушателя с любого элемента DELETE
+function removeAnyListeners(element, eventString, func) {
+  element.removeEventListener(eventString, func);
+}
+
+function openPopupImg(link, name) {
+  bigImg.src = link;
+  bigImg.alt = name;
+  captionBigImg.textContent = name;
+  openPopup(popupImg);
+}
 
 
 // buttonEdt.addEventListener("click", () => {
@@ -165,19 +172,22 @@ function addCard(container, cardElement, append = false) {
 // // buttonClsImg.addEventListener("click", () => {closePopup(popupImg)}); //DELETE
 // formElement.addEventListener("submit", formSubmitHandler);
 // // formElementAdd.addEventListener("submit", formAddSubmitHandler);
-buttonClsImg.addEventListener("click", () => {closePopup(popupImg)}); //DELETE
+buttonClsImg.addEventListener("click", () => {closePopup(popupImg)});
 
-// //Добавление слушателей на все модальный окна для их закрытия по оверлэю
-// popups.forEach(popup => {
-//   popup.addEventListener("click", evt => {
-//     if (evt.target.classList.contains("popup")) {
-//       closePopup(evt.target);
-//     }
-//   });
-// });
+
+//Добавление слушателей на все модальный окна для их закрытия по оверлэю
+popups.forEach(popup => {
+  popup.addEventListener("click", evt => {
+    if (evt.target.classList.contains("popup")) {
+      closePopup(evt.target);
+    }
+  });
+});
+
 //Заполнение карточками страницы при ее загрузке
 initialCards.forEach(item => {
-  const card = new Card(item, '.elements__template');
+  const card = new Card(item, ".elements__template");
   const cardElement = card.generateCard();
+  card.buttonImgElement.addEventListener("click", () => {openPopupImg(card.link, card.name)});
   addCard(elementsContainer, cardElement, true);
 });
