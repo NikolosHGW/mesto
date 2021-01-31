fe41a1c845a1b6004552b24c6ccf0b194e1d09e9
Ник Николос <nic23172@gmail.com>
2021-01-31 18:00:43 +0300

    refactor: create class PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,27 @@
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, formSubmitHandler) {
+    super(popupSelector);
+    this._form = this._popup.querySelector('.popup__form');
+    this._formSubmitHandler = formSubmitHandler;
+  }
+
+  _getInputValues() {
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    this._formValues = {};
+    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', evt => {
+      evt.preventDefault();
+      this._formSubmitHandler(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
