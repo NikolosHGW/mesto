4a4cbeb4e0d36e308e866ace3c06bd658d5061a6
Ник Николос <nic23172@gmail.com>
2021-02-02 12:31:07 +0300

    refactor: delete superfluous

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
 import FormValidator from "../components/FormValidator.js";
 import {
-  body, popups, popupEdt, popupImg, buttonEdt, buttonAdd,
-  formElementEdt, formElementAdd, nameInput, jobInput, cardNameInput,
-  imgLinkInput, profName, profJob, bigImg, captionBigImg, elementsContainer, config,
-  initialCards
+  body, buttonEdt, buttonAdd,
+  formElementEdt, formElementAdd, nameInput, jobInput,
+  bigImg, captionBigImg, config, initialCards
 } from '../utils/constants.js';
 
 
 const formEdtValid = new FormValidator(config, formElementEdt);
 const formAddValid = new FormValidator(config, formElementAdd);
+
 const elementsSection = new Section( { items: initialCards, renderer: item => {
   const card = new Card(item, '.elements__template', (link, nameCard) => {
     const popupImage = new PopupWithImage('.popup_img', body, { link: link, nameCard: nameCard });
@@ -42,8 +42,6 @@ const popupAdd = new PopupWithForm('.popup_add', body, item => {
 });
 
 
-popupEdit.setEventListeners();
-popupAdd.setEventListeners();
 buttonEdt.addEventListener('click', () => {
   popupEdit.open();
   nameInput.value = userInfo.getUserInfo().name;
@@ -54,129 +52,8 @@ buttonAdd.addEventListener('click', () => {
   formAddValid.resetValidation();
   popupAdd.open();
 });
+popupEdit.setEventListeners();
+popupAdd.setEventListeners();
 elementsSection.renderItems();
 formEdtValid.enableValidation();
 formAddValid.enableValidation();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const formEdtValid = new FormValidator(config, formElementEdt);
-// const formAddValid = new FormValidator(config, formElementAdd);
-
-
-// //Функция для открытия Popup окон
-// function openPopup(popup) {
-//   popup.classList.add("popup_opened"); //добавляет стиль с visibility:visible, чтобы перекрыть visibility:hide
-//   addAnyListeners(body, "keydown", closePopupWithEscape);
-// }
-
-// //Функция для закрытия Popup окон
-// function closePopup(popup) {
-//   popup.classList.remove("popup_opened"); // убирает модификатор со стилем visibility:visible
-//   removeAnyListeners(body, "keydown", closePopupWithEscape);
-// }
-
-// //Функция для отправки данных из инпут полей PopupEdt в profile
-// function formSubmitHandler(evt) {
-//   evt.preventDefault(); // сбрасывает стандартную отправку формы
-
-//   profName.textContent = nameInput.value;
-//   profJob.textContent = jobInput.value;
-//   closePopup(popupEdt);
-// }
-
-// //Функция для добавления карточки в контейнер
-// function addCard(container, cardElement, append = false) {
-//   append ? container.append(cardElement) : container.prepend(cardElement);
-// }
-
-// function creatCard(data, templateSelector = ".elements__template") {
-//   const card = new Card(data, templateSelector, openPopupImg);
-//   const cardElement = card.generateCard();
-//   return cardElement;
-// }
-
-// //Функция для добавлении новых карточек
-// function formAddSubmitHandler(evt) {
-//   evt.preventDefault(); // сбрасывает стандартную отправку формы
-
-//   addCard(elementsContainer, creatCard({ name: cardNameInput.value, link: imgLinkInput.value }));
-//   closePopup(popupAdd);
-// }
-
-// //Функция для закрытия попапа при событии keydown равное esc
-// function closePopupWithEscape(evt) {
-//   if (evt.key == "Escape") {
-//     const openedPopup = document.querySelector('.popup_opened')
-//     if (openedPopup) {
-//       closePopup(openedPopup);
-//     }
-//   }
-// }
-
-// //Функция для добавления слушателя на любой элемент
-// function addAnyListeners(element, eventString, func) {
-//   element.addEventListener(eventString, func);
-// }
-
-// //Функция для удаления слушателя с любого элемента
-// function removeAnyListeners(element, eventString, func) {
-//   element.removeEventListener(eventString, func);
-// }
-
-// function openPopupImg(link, name) {
-//   bigImg.src = link;
-//   bigImg.alt = name;
-//   captionBigImg.textContent = name;
-//   openPopup(popupImg);
-// }
-
-
-// buttonEdt.addEventListener("click", () => {
-//   nameInput.value = profName.textContent;
-//   jobInput.value = profJob.textContent;
-//   formEdtValid.resetValidation();
-//   openPopup(popupEdt);
-// });
-// buttonAdd.addEventListener("click", () => {
-//   formElementAdd.reset();
-//   formAddValid.resetValidation();
-//   openPopup(popupAdd);
-// });
-// formElementEdt.addEventListener("submit", formSubmitHandler);
-// formElementAdd.addEventListener("submit", formAddSubmitHandler);
-
-
-// formEdtValid.enableValidation();
-// formAddValid.enableValidation();
-
-// //Добавление слушателей на все модальный окна для их закрытия по оверлэю
-// popups.forEach(popup => {
-//   popup.addEventListener("click", evt => {
-//     if (evt.target.classList.contains("popup")) {
-//       closePopup(evt.target);
-//     }
-//     if (evt.target.classList.contains("popup__close-icon")) {
-//       closePopup(popup);
-//     }
-//   });
-// });
-
-// //Заполнение карточками страницы при ее загрузке
-// initialCards.forEach(item => {
-//   addCard(elementsContainer, creatCard(item), true);
-// });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,14 @@
 export const body = document.querySelector(".body");
-export const popups = Array.from(document.querySelectorAll(".popup"));
 export const popupEdt = document.querySelector(".popup_edd");
 export const popupAdd = document.querySelector(".popup_add");
-export const popupImg = document.querySelector(".popup_img");
 export const buttonEdt = document.querySelector(".profile__edit-button");
 export const buttonAdd = document.querySelector(".profile__add-button");
-export const profile = document.querySelector(".profile");
 export const formElementEdt = popupEdt.querySelector(".popup__form");
 export const formElementAdd = popupAdd.querySelector(".popup__form");
 export const nameInput = popupEdt.querySelector(".popup__input_el_name");
 export const jobInput = popupEdt.querySelector(".popup__input_el_job");
-export const cardNameInput = popupAdd.querySelector(".popup__input_el_card-name");
-export const imgLinkInput = popupAdd.querySelector(".popup__input_el_img-link");
-export const profName = profile.querySelector(".profile__name");
-export const profJob = profile.querySelector(".profile__job");
-export const bigImg = popupImg.querySelector(".popup-img__img");
-export const captionBigImg = popupImg.querySelector(".popup-img__caption");
-export const elementsContainer = document.querySelector(".elements");
+export const bigImg = document.querySelector(".popup-img__img");
+export const captionBigImg = document.querySelector(".popup-img__caption");
 export const config = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
